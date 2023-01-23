29d528b8c01f5993db7f6f78e6df1cb40c27ff9f
Erin Rosien <elrosien@gmail.com>
2023-01-22 20:01:17 -0800

    first commit

---
+++ new_module.py
@@ -0,0 +1,12 @@
+#1
+def sqft(length, width):
+    area = int(length) * int(width)
+    print(f"The square footage of the house is {area}.")
+
+#2
+from math import pi
+
+def circumference(diameter):
+    c = int(diameter) * pi
+    print(f"The circle's circumference is {c}.")
+    
